64b13b8af3f6dc94dfaf97b0cb8efcc3fb356dcb
Jaehyun Lee <myleje@gmail.com>
2020-01-16 17:02:55 +0900

    .

--- Assets/02_Scripts/MoveCtrl.cs
+++ Assets/02_Scripts/MoveCtrl.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class MoveCtrl : MonoBehaviour
 {
+    public Transform camTr;    //하위에 있는 Main Camera의 Transform 컴포넌트
+    private CharacterController cc;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        camTr = Camera.main.transform;  //Camera.main  --> 'MainCamera' 태그가 설정된 Camera 리턴
+        cc    = this.gameObject.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
