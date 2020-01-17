5d6ae049297887cd143a4b5d9ca20fa6df879cc3
Jaehyun Lee <myleje@gmail.com>
2020-01-17 14:41:53 +0900

    .

---
+++ Assets/02_Scripts/EyeCast.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeCast : MonoBehaviour
+{
+    private Transform tr;   //자신의 Transform 컴포넌트를 저장할 변수
+    private Ray ray;        //생성할 광선
+    private RaycastHit hit; //광선끝에 충돌한 충돌정보를 저장할 구조체
+
+    [Range(5.0f, 20.0f)]
+    public float distance = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
