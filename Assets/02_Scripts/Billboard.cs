c4cfaa9d40650b02fab94db259b0d2aa0e5a5123
Jaehyun Lee <myleje@gmail.com>
2020-01-17 13:49:14 +0900

    .

---
+++ Assets/02_Scripts/Billboard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    [SerializeField]
+    private Transform camTr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camTr = Camera.main.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
